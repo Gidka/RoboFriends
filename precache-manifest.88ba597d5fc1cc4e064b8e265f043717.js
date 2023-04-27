4d3278b09d6e60c565a1a3d833a9e3097858c521
Gidka <gidka1998@gmail.com>
2023-04-27 10:53:26 +0200

    Updates

---
+++ precache-manifest.88ba597d5fc1cc4e064b8e265f043717.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c164d61926c06cce29b1",
+    "url": "/robofriends/static/css/main.0d471d33.chunk.css"
+  },
+  {
+    "revision": "c164d61926c06cce29b1",
+    "url": "/robofriends/static/js/main.c164d619.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "fa3fb3c98559d2dceee3",
+    "url": "/robofriends/static/css/2.b14a0a7a.chunk.css"
+  },
+  {
+    "revision": "fa3fb3c98559d2dceee3",
+    "url": "/robofriends/static/js/2.fa3fb3c9.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f857d1c5539b85966e8f1d287f0267b0",
+    "url": "/robofriends/index.html"
+  }
+];
